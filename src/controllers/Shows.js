112dc93e6bf5ebd84048ee2cf898138ef6920d1e
luzialeticia <leticialuzia@tutamail.com>
2020-11-06 22:24:18 -0300

    Criando função getById e update

--- src/controllers/Shows.js
+++ src/controllers/Shows.js
@@ -11,8 +11,60 @@ const getShows = (req, res) => {
   })
 }
 
+const getShowById = (req, res) => {
+  const id = req.params.id
+  model.showsCollection.findById(id, (error, show) => {
+    if(error) {
+      return res.status(500).send(error)
+    } else {
+      if(show) {
+        return res.status(200).send(show)
+      } else {
+        return.status(404).send('Show not found.')
+      }
+    }
+  })
+}
 
+const addShow = (req, res) => {
+  console.log(req.url)
+  const showBody = req.body
+  const show = new model.showsCollection(showBody)
+
+  show.save((error) => {
+    if(error) {
+      return res.status(400).send(error)
+    } else {
+      return res.status(201).send(show)
+    }
+  })
+}
+
+const updateShow = (req, res) => {
+  const id = req.params.id
+  const showBody = req.body
+  const updatedShow = {new: true}
+
+  model.showsCollection.findByIdAndUpdate(
+    id,
+    {$set:{showBody}},
+    updatedShow,
+    (error, show) => {
+      if(error) {
+        return res.status(500).send(error)
+      } else if(show) {
+        return res.status(200).send(updatedShow)
+      } else {
+        return res.sendStatus(400)
+      }
+    }
+  )
+}
 
 module.exports = {
-  getShows
+  getShows,
+  getShowById,
+  addShow,
+  updateShow,
+  deleteShow
 }
