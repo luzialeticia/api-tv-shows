eb85dbb771985fe5fd9b1a43a889ff9dc66ca9b4
luzialeticia <leticialuzia@tutamail.com>
2020-11-06 22:32:27 -0300

    Criand as rotas

--- src/routes/Shows.js
+++ src/routes/Shows.js
@@ -3,6 +3,10 @@ const router = express.Router()
 const controller = require('../controllers/Shows')
 const cors = require('cors')
 
-
+router.get('/shows', cors(), controller.getShows)
+router.get('/shows/:id', cors(), controller.getShowById)
+router.post('/shows', cors(), controller.addShow)
+router.put('/shows/:id', cors(), controller.updateShow)
+router.delete('/shows/:id', cors(), controller.deleteShow)
 
 module.exports = router
